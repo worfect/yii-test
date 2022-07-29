1615e595704c4c2dcef314c11670f2f56fe6099d
Max Senchenkov <senchenkov.93@gmail.com>
2022-07-30 04:13:41 +0800

    init psalm, csfixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+return
+    (new PhpCsFixer\Config())
+        ->setCacheFile(__DIR__ . '/storage/cache/..php-cs-fixer.php')
+        ->setFinder(
+            PhpCsFixer\Finder::create()
+                ->in([
+                    __DIR__ . '/console/migrations',
+                    __DIR__ . '/common/fixtures',
+                    __DIR__ . '/common/models',
+                    __DIR__ . '/frontend/controllers',
+                    __DIR__ . '/frontend/views',
+                ])
+                ->append([
+                    __FILE__,
+                ])
+        )
+        ->setRules([
+            '@PSR12' => true,
+            '@PSR12:risky' => true,
+            '@PhpCsFixer' => true,
+            '@PhpCsFixer:risky' => true,
+            '@PHP81Migration' => true,
+
+            'ordered_imports' => ['imports_order' => ['class', 'function', 'const']],
+
+            'concat_space' => ['spacing' => 'one'],
+
+            'phpdoc_to_comment' => false,
+            'phpdoc_separation' => false,
+            'phpdoc_types_order' => ['null_adjustment' => 'always_last'],
+            'phpdoc_align' => false,
+
+            'operator_linebreak' => false,
+
+            'global_namespace_import' => true,
+
+            'blank_line_before_statement' => false,
+            'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
+
+            'fopen_flags' => ['b_mode' => true],
+
+            'yoda_style' => false,
+
+            'final_class' => true,
+            'self_static_accessor' => true,
+
+            'static_lambda' => true,
+        ]);
--- Makefile
+++ Makefile
@@ -10,8 +10,20 @@ d-build:
 d-down-clear:
 	docker-compose down -v --remove-orphans
 
-composer-upd:
-	docker-compose run --rm cli composer update
+m-r-s:
+	docker-compose run --rm backend yii  migrate/down all --interactive=0
+	docker-compose run --rm backend yii migrate --interactive=0
+	docker-compose run --rm backend yii fixture/load "*" --namespace='common\fixtures' --interactive=0
+
+psalm:
+	docker-compose run --rm backend ./vendor/bin/psalm
+
+lint:
+	docker-compose run --rm backend composer php-cs-fixer fix -- --dry-run --diff
+
+fix:
+	docker-compose run --rm backend composer php-cs-fixer fix
+
 #
 #cache-clear:
 #	docker-compose run --rm cli php artisan optimize:clear
